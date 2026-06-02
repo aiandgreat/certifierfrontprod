859a73eddddab9d9562119d97002a253e3b6b52c
aiandgreat <ajsgarcia.student@ua.edu.ph>
2026-06-02 20:33:46 +0800

    reverted

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const normalizedApiBase = import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, '');
 
-export const API_BASE = normalizedApiBase || 'https://certifier-backend-b2ne.onrender.com';
+export const API_BASE = normalizedApiBase || 'http://127.0.0.1:8000';
 
 export default API_BASE;
